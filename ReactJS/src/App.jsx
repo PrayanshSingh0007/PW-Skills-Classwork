92aeff9c36433153512fbb49fc9b5727c675850e
Prayansh <Pradyvisen@gmail.com>
2024-09-28 12:12:12 +0530

    28 September

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const [isValid, setIsValid] = useState(false)
+
+//   return (
+//     <div>
+//       <h1>{isValid ? count: "You are Invalid"}</h1>
+//       <button onClick={() => setCount(count + 1)}>
+//       Increment
+//       </button>
+//       <button onClick={() => setCount(count - 1)}>
+//       Decrement
+//       </button>
+//       <button onClick={() => setIsValid(!isValid)}>
+//       Toggle
+//       </button>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   const [theme, setTheme] = useState('light')
+//   function handletheme () {
+//      if (theme === 'light') {
+//        setTheme('dark')
+//      } else {
+//        setTheme('light')
+//      }
+//   }
+
+//   function fun (){
+//     return (
+//       <div>
+//         <h1>Theme changed</h1>
+//       </div>
+//     )
+//   }
+//   useEffect(() => {
+//     fun()
+//   }, [theme])
+
+//   return (
+//     <div 
+//     className="App"
+//     style={{backgroundColor: theme === "light" ? "#ffffff" : "#000000"}}>
+//     <button onClick={handletheme}> Theme Change</button>
+//     </div>
+// }
+
+// export default App
+
+// sir code 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("light");
+  const [counter, setCounter] = useState(0);
+  function handleTheme() {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+  function handleCounter() {
+    setCounter((c) => c + 1);
+  }
+
+  useEffect(() => {
+    var interval = setInterval(() => {
+      setCounter((c) => c + 1);
+    }, 1000);
+    console.log(counter);
+    return () => {
+      clearInterval(interval);
+      console.log("Unmounting & clear up function");
+    };
+  }, [theme, counter]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className="App"
+      // style={{ backgroundColor: theme == "light" ? "#ffffff" : "#000000" }}
+    >
+      <h1>{counter}</h1>
+      <button onClick={handleTheme}>Theme Change</button>
+      <button onClick={handleCounter}>Counter</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
